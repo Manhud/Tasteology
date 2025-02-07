01146a510fb53765efc1a0a40012a4849d0b651f
Juan Rozo <rozoacwow@gmail.com>
2025-02-07 12:57:50 -0500

    chore: add commitlint and husky dependencies

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New features
+        'fix',      // Bug fixes
+        'docs',     // Documentation changes
+        'style',    // Code style changes (formatting, etc)
+        'refactor', // Code refactoring
+        'perf',     // Performance improvements
+        'test',     // Adding or modifying tests
+        'chore',    // Maintenance tasks
+        'ci',       // CI/CD changes
+      ],
+    ],
+  },
+}; 
