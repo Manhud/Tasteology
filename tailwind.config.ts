8e3919b764a8330431c62b1f57ff176594670d29
Juan Rozo <rozoacwow@gmail.com>
2025-02-07 16:56:29 -0500

    feat(theme): add red color to design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        red: "var(--red)",
       },
       keyframes: {
         "fade-in": {
