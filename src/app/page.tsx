0a4d5299038be71d27d1cb9193ceef6b804c89d9
Juan Rozo <rozoacwow@gmail.com>
2025-02-07 18:04:29 -0500

    feat(page): add taste colors section to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { CardBlock } from "@/components/card-block";
 import { HeroBlock } from "@/components/hero-block";
 
 export default function Home() {
   return (
     <main>
       <HeroBlock />
+      <CardBlock />
     </main>
   );
 }
